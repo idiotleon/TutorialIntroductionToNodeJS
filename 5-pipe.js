f4020dab577abd4889aaaab37457e6c3c0070e76
Yang Lu <y80053445@china.huawei.com>
2017-12-29 17:40:54 -0800

    demo example: Piping between Streams
    Module 3.7, part 1

--- 5-pipe.js
+++ 5-pipe.js
@@ -1,6 +1,11 @@
 // Module 3.7: Demo of Piping between Streams
 var request = require('request');
 
+/*
 var s = request('http://www.pluralsight.com');
 
 s.pipe(process.stdout);
+*/
+
+// another way to express above logic: chain them together
+request('http://www.pluralsight.com/').pipe(process.stdout);
