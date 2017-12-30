88a019e686ec4d9ef6dd73f185c82b4bcc125324
Yang Lu <y80053445@china.huawei.com>
2017-12-29 17:30:34 -0800

    Revert "demo example: Readable and Writable Streams"
    This reverts commit c3610513edd481aeb1381f77499341991a97dc28.

--- 4-writable-stream.js
+++
@@ -1,5 +0,0 @@
-// Module 3.6: Demo of writable stream
-console.log("stdout is writable? " + process.stdout.writable);
-
-process.stdout.write("Hello");
-process.stdout.write("World");
--- 5-pipe.js
+++
@@ -1,6 +0,0 @@
-// Module 3.7: Demo of Piping between Streams
-var request = require('request');
-
-var s = request('http://www.pluralsight.com');
-
-s.pipe(process.stdout);
